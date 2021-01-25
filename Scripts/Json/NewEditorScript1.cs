57c1769650a9042459bc862236fa98e81164ffd9
Juan Jose Vargas Fletes <androidapps0703@gmail.com>
2021-01-25 01:27:11 -0600

    Added login scene. TODO: Validate fields also move Network class to this scene

---
+++ Scripts/Json/NewEditorScript1.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Fighter
+{
+    public string name { get; set; }
+    public string attack { get; set; }
+    public int porc { get; set; }
+    public int power { get; set; }
+    public int res { get; set; }
+    public int health { get; set; }
+}
---
+++ Scripts/Login/FighterGenerator.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FighterGenerator : MonoBehaviour
+{
+    public Sprite[] FightersImage;
+    public string[] FightersName;
+    public string[] FightersAttacks;
+    public int[] FightersPorc;
+
+    private int amountFighters;
+    private int[] values = new int[] { 100, 75, 50};
+
+    private Transform availableContainer;
+    private Transform selectedContainer;
+
+    private Button playButton;
+    private Text playerName;
+
+    private Dictionary<string, Fighter> warriors;
+
+    void Start()
+    {
+        warriors = new Dictionary<string, Fighter>();
+
+        availableContainer = GameObject.Find("AvailableContent").GetComponent<Transform>();
+        selectedContainer = GameObject.Find("SelectedContainer").GetComponent<Transform>();
+        playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+        playerName = GameObject.Find("TextName").GetComponent<Text>();
+
+
+        playButton.onClick.AddListener(delegate {
+            OnPlayButton();
+        });
+
+        amountFighters = FightersImage.Length;
+
+        generatFighters();
+    }
+    void Update()
+    {
+        
+    }
+
+    private void OnPlayButton()
+    {
+        Debug.Log("Nombre del jugador: " + playerName.text);
+        // TODO: Conectar con el back
+    }
+
+    // Aqui generamos los luchadores disponibles
+    private void generatFighters()
+    {
+        GameObject reference = (GameObject)Instantiate(Resources.Load("Fighters"));
+
+        for (int i = 0; i < amountFighters; i++)
+        {
+            GameObject fighter = Instantiate(reference, availableContainer);
+            
+            Text name = fighter.transform.Find("FighterName").GetComponent<Text>();
+            Text porc = fighter.transform.Find("FighterPorc").GetComponent<Text>();
+            Image image = fighter.transform.Find("FighterImage").GetComponent<Image>();
+            Button button = fighter.transform.Find("FighterButton").GetComponent<Button>();
+
+            image.sprite = FightersImage[i];
+            name.text = FightersName[i];
+            porc.text = "Porcentaje: " + FightersPorc[i].ToString() + "%";
+
+            int localIndex = i;
+
+            button.onClick.AddListener(delegate () {
+                int index = localIndex;
+
+                generateSelected(index);
+            });
+        }
+
+        Destroy(reference);
+    }
+
+    // Aqui agregamos los jugadores que vamos a usar
+    private void generateSelected(int index)
+    {
+        if (warriors.ContainsKey(FightersName[index]))
+        {
+            // Trigger global message
+            return;
+        }
+
+        if (warriors.Count >= 3)
+        {
+            // Trigger global message
+
+            return;
+        }
+
+        GameObject fighter = (GameObject)Instantiate(Resources.Load("SelectedFighter"), selectedContainer);
+
+        Text name = fighter.transform.Find("FighterName").GetComponent<Text>();
+        Text porc = fighter.transform.Find("FighterPorc").GetComponent<Text>();
+
+        Image image = fighter.transform.Find("FighterImage").GetComponent<Image>();
+        Dropdown dpPower = fighter.transform.Find("DPPower").GetComponent<Dropdown>();
+        Dropdown dpHealth = fighter.transform.Find("DPHealth").GetComponent<Dropdown>();
+        Dropdown dpRes = fighter.transform.Find("DPRes").GetComponent<Dropdown>();
+        Dropdown dpAttack = fighter.transform.Find("DPAttack").GetComponent<Dropdown>();
+
+        name.text = FightersName[index];
+        image.sprite = FightersImage[index];
+        porc.text = "Porcentaje: " + FightersPorc[index].ToString() + "%";
+
+        Fighter fighterRef = new Fighter();
+
+        fighterRef.name = name.text;
+        fighterRef.porc = FightersPorc[index];
+        // TODO: Cambiar estos por uno de obtener libres
+        fighterRef.power = values[0];
+        fighterRef.res = values[0];
+        fighterRef.health = values[0];
+        fighterRef.attack = FightersAttacks[0];
+
+        dpPower.onValueChanged.AddListener(delegate {
+            fighterRef.power = values[dpPower.value];
+        });
+
+        dpHealth.onValueChanged.AddListener(delegate {
+            fighterRef.health = values[dpPower.value];
+        });
+
+        dpRes.onValueChanged.AddListener(delegate {
+            fighterRef.res = values[dpPower.value];
+        });
+
+        dpAttack.onValueChanged.AddListener(delegate {
+            fighterRef.attack = FightersAttacks[dpAttack.value];
+        });
+
+        warriors.Add(name.text, fighterRef);
+    }
+}
